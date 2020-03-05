ea2b905d6d76f72787c2c92077229f9a2d4ca92b
JustDoWhitt <Chjwhitt96@gmail.com>
2020-03-05 18:41:43 -0500

    Basic Personal Website

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageComponent } from './page/page.component';
+
+
+import { ContentService } from './shared/services/content.service';
+import { AppRoutingModule } from './app-routing.module';
+import { FullpageDirective } from './shared/directives/fullpage.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageComponent,
+    FullpageDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    AppRoutingModule
+  ],
+  providers: [ContentService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
---
+++ src/app/shared/services/content.service.ts
@@ -0,0 +1,22 @@
+// app/shared/services/content.service.ts
+
+
+
+import { Injectable } from '@angular/core';
+
+
+
+@Injectable()
+export class ContentService {
+
+  pages: Object = {
+
+    'home': {title: 'Home', subtitle: 'Welcome!', content: "You've arrived to my website. TheChillGamers is a work in progress that currently serves as an about me. I will continue to add further functionality and content regarding other projects that I work on.", image: 'assets/background.png'},
+
+    'about': {title: 'About', subtitle: 'About Me', content: "I'm just a guy with a passion for technology and programming. I love to learn new programming languages which is why I started this website as a way to become familiar with the Angular framework. I'm eager and determined to learn and grow as a developer.", image: 'assets/background.png'},
+
+    'contact': {title: 'Contact', subtitle: 'Contact Us', content: 'justdoinwhitt@gmail.com', content2: 'Phone number is available upon request.', content3: 'LinkedIn', image: 'assets/background.png'}
+
+  };
+
+}
